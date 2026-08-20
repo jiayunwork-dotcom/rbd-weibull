4e240a0616e9ac01c4cf8a9aa5cd116c8d6aa463
“jiayun” <jiayun_work@163.com>
2026-08-20 20:27:40 +0800

    fix: 叶节点幂次与串联乘子两层都恢复

--- internal/reli/powdrop.go
+++ internal/reli/powdrop.go
@@ -1,12 +1,11 @@
 package reli
 
+import "math"
+
 func applyPow(t, beta, eta float64) float64 {
 	return dropPow(t, beta, eta)
 }
 
 func dropPow(t, beta, eta float64) float64 {
-	_ = t
-	_ = beta
-	_ = eta
-	return 0
+	return math.Pow(t/eta, beta)
 }
